d5a620c91fb8e74983aec0024b7524b3ecfd1d1a
Alberto Scala <alberto.scala.it@gmail.com>
2026-09-02 22:05:46 +0200

    starting handover

---
+++ src/jny_core.c
@@ -0,0 +1,198 @@
+#define DEBUG
+
+#include <stdint.h>
+
+#include "../include/csr.h"
+#include "../include/qemu.h"
+#include "../include/extensions.h"
+
+// Data section addresses
+extern uintptr_t _data_start;
+extern uintptr_t _data_end;
+// BSS section addresses
+extern uintptr_t _bss_start;
+extern uintptr_t _bss_end;
+// Trap stack top address
+extern uintptr_t _trap_stack_top;
+// Trap handler address
+extern uintptr_t trap_handler;
+
+void jny_panic(const char* msg, uint64_t mepc, uint64_t mcause, uint64_t code)
+{
+    uart_write_string("---- PANIC ----");
+    uart_write_string("bootloader panic: "); uart_write_string(msg); uart_write_string("\n");
+    uart_write_string("mepc= "); uart_write_hex(mepc); uart_write_string("\t");
+    uart_write_string("mcause= "); uart_write_hex(mcause); uart_write_string("\n");
+    uart_write_string("---------------");
+
+    // Exit non-zero from qemu
+    SIFIVE_TEST_FAIL(code); 
+}
+
+void jny_logo()
+{
+    uart_write_string("\n");
+    uart_write_string(" __\n");
+    uart_write_string("/\\_\\    ___   __  __\n");
+    uart_write_string("\\/\\ \\ /' _ `\\/\\ \\/\\ \\\n");
+    uart_write_string(" \\ \\ \\/\\ \\/\\ \\ \\ \\_\\ \\\n");
+    uart_write_string(" _\\ \\ \\ \\_\\ \\_\\/`____ \\\n");
+    uart_write_string("/\\ \\_\\ \\/_/\\/_/`/___/> \\\n");
+    uart_write_string("\\ \\____/          /\\___/\n");
+    uart_write_string(" \\/___/           \\/__/\n");
+    uart_write_string("                __                      __    ___                        __\n");
+    uart_write_string("               /\\ \\                    /\\ \\__/\\_ \\                      /\\ \\\n");
+    uart_write_string("               \\ \\ \\____    ___     ___\\ \\ ,_\\//\\ \\     ___      __     \\_\\ \\     __   _ __\n");
+    uart_write_string("                \\ \\ '__`\\  / __`\\  / __`\\ \\ \\/ \\ \\ \\   / __`\\  /'__`\\   /'_` \\  /'__`\\/\\`'__\\\n");
+    uart_write_string("                 \\ \\ \\L\\ \\/\\ \\L\\ \\/\\ \\L\\ \\ \\ \\_ \\_\\ \\_/\\ \\L\\ \\/\\ \\L\\.\\_/\\ \\L\\ \\/\\  __/\\ \\ \\/\n");
+    uart_write_string("                  \\ \\_,__/\\ \\____/\\ \\____/\\ \\__\\/\\____\\ \\____/\\ \\__/.\\_\\ \\___,_\\ \\____\\\\ \\_\\\n");
+    uart_write_string("                   \\/___/  \\/___/  \\/___/  \\/__/\\/____/\\/___/  \\/__/\\/_/\\/__,_ /\\/____/ \\/_/\n");
+    uart_write_string("\n");
+}
+
+void jny_bss_init()
+{
+    uintptr_t* curr = &_bss_start;
+    while (curr < &_bss_end)
+    {
+        *curr = 0;
+        curr++;
+    }
+}
+
+void jny_data_init()
+{
+    /* Skip; Not needed yet */
+}
+
+void jny_mscratch_init()
+{
+    CSRW(MSCRATCH, &_trap_stack_top);
+
+#ifdef DEBUG
+    uint64_t check; CSRR(check, MSCRATCH);
+    uart_write_string("[jny_mscratch_init] mscratch="); uart_write_hex(check); uart_write_string("\n");
+#endif
+
+}
+
+void jny_mtvec_init()
+{
+    CSRW(MTVEC, &trap_handler);
+
+#ifdef DEBUG
+    uint64_t check; CSRR(check, MTVEC);
+    uart_write_string("[jny_mtvec_init] mtvec="); uart_write_hex(check); uart_write_string("\n");
+#endif
+
+}
+
+typedef struct {
+    uint64_t x1;
+    uint64_t x3;
+    uint64_t x4;
+    uint64_t x5;
+    uint64_t x6;
+    uint64_t x7;
+    uint64_t x8;
+    uint64_t x9;
+    uint64_t x10;
+    uint64_t x11;
+    uint64_t x12;
+    uint64_t x13;
+    uint64_t x14;
+    uint64_t x15;
+    uint64_t x16;
+    uint64_t x17;
+    uint64_t x18;
+    uint64_t x19;
+    uint64_t x20;
+    uint64_t x21;
+    uint64_t x22;
+    uint64_t x23;
+    uint64_t x24;
+    uint64_t x25;
+    uint64_t x26;
+    uint64_t x27;
+    uint64_t x28;
+    uint64_t x29;
+    uint64_t x30;
+    uint64_t x31;
+    uint64_t mcause;
+    uint64_t mepc;
+} frame_t;
+
+#define CAUSE_BIT   (1ULL << 63)
+#define CAUSE_MASK  ((1ULL << 63)-1)
+
+void jny_dispatcher(frame_t* frame)
+{
+    uint64_t code = frame->mcause & CAUSE_MASK;
+
+    switch (frame->mcause & CAUSE_BIT)
+    {
+    case 0:                 // Exception path
+        switch (code)
+        {
+        case 0: jny_panic("instruction address misaligned", frame->mepc, frame->mcause, code); break;
+        case 1: jny_panic("instruction access fault", frame->mepc, frame->mcause, code); break;
+        case 2: jny_panic("illegal instruction", frame->mepc, frame->mcause, code); break;
+        case 3: jny_panic("breakpoint (unexpected)", frame->mepc, frame->mcause, code); break;
+        case 4: jny_panic("load address misaligned", frame->mepc, frame->mcause, code); break;
+        case 5: jny_panic("load access fault", frame->mepc, frame->mcause, code); break;
+        case 6: jny_panic("store/amo address misaligned", frame->mepc, frame->mcause, code); break;
+        case 7: jny_panic("store/amo access fault", frame->mepc, frame->mcause, code); break;
+        case 8: jny_panic("ecall from u-mode (unexpected - no priv drop yet)", frame->mepc, frame->mcause, code); break;
+        case 9: jny_panic("ecall from s-mode (unexpected - no priv drop yet)", frame->mepc, frame->mcause, code); break;
+        case 11:            // ecall from M-mode - your deliberate test trap
+            uart_write_string("[jny_dispatcher] ecall from m-mode, mepc="); uart_write_hex(frame->mepc); uart_write_string("\n");
+            CSRW(MEPC, frame->mepc + 4); // must advance, or you re-trap on the same ecall forever   
+            break;
+        case 12: jny_panic("instruction page fault (unexpected - no paging yet)", frame->mepc, frame->mcause, code); break;
+        case 13: jny_panic("load page fault (unexpected - no paging yet)", frame->mepc, frame->mcause, code); break;
+        case 15: jny_panic("store/amo page fault (unexpected - no paging yet)", frame->mepc, frame->mcause, code); break;
+        default: jny_panic("unhandled exception code", frame->mepc, frame->mcause, 255); break;
+        }
+        break;
+    case CAUSE_BIT:         // Interrupt path
+        switch (code)
+        {
+        case 3:  jny_panic("machine software interrupt (unexpected - MIE off)", frame->mepc, frame->mcause, code); break;
+        case 7:  jny_panic("machine timer interrupt (unexpected - MIE off)", frame->mepc, frame->mcause, code); break;
+        case 11: jny_panic("machine external interrupt (unexpected - MIE off)", frame->mepc, frame->mcause, code); break;
+        default: jny_panic("unhandled interrupt code", frame->mepc, frame->mcause, 255); break;
+        }
+        break;
+    default: jny_panic("unreachable mcause category", frame->mepc, frame->mcause, 255); break;
+    }
+}
+
+#define DISPATCHER_TEST
+
+void jny_main()
+{
+    // Logo
+    jny_logo();
+
+    // Zeroing the BSS sections
+    jny_bss_init();
+
+    // Moving data from ROM to RAM
+    jny_data_init();
+
+    // Set the trap stack top
+    jny_mscratch_init();
+
+    // Set the trap handler address
+    jny_mtvec_init();
+
+#ifdef DISPATCHER_TEST
+    asm volatile("ecall");
+    uart_write_string("[jny_main] dispatcher test passed\n");
+#endif
+
+    // Success
+
+    // TEST PASSED (EXIT)
+    SIFIVE_TEST_PASS();
+}
---
+++ src/jny_init.s
@@ -0,0 +1,91 @@
+.section .text.init
+.global _bstart
+.global trap_handler
+
+_bstart:
+    csrr t0, mhartid        # get the mhartid
+    bnez t0, park_hart      # if it is not 0 disable
+    la sp, _stack_top       # initialize the stack stack
+    call jny_main           # jump into C part of the bootloader
+
+park_hart:
+    wfi                     # wait until the next interrupt
+    j park_hart             # stay parked
+
+.align 4
+trap_handler:
+    csrrw sp, mscratch, sp  # swap to trap stack
+    addi sp, sp, -256       # reserve stack for 32 registers (30 because x0(ra) and x2(sp) are not counted but we need to be aligned to 16 bytes)
+    # Boring stack saving
+    sd x1, 0(sp)
+    sd x3, 8(sp)
+    sd x4, 16(sp)
+    sd x5, 24(sp)
+    sd x6, 32(sp)
+    sd x7, 40(sp)
+    sd x8, 48(sp)
+    sd x9, 56(sp)
+    sd x10, 64(sp)
+    sd x11, 72(sp)
+    sd x12, 80(sp)
+    sd x13, 88(sp)
+    sd x14, 96(sp)
+    sd x15, 104(sp)
+    sd x16, 112(sp)
+    sd x17, 120(sp)
+    sd x18, 128(sp)
+    sd x19, 136(sp)
+    sd x20, 144(sp)
+    sd x21, 152(sp)
+    sd x22, 160(sp)
+    sd x23, 168(sp)
+    sd x24, 176(sp)
+    sd x25, 184(sp)
+    sd x26, 192(sp)
+    sd x27, 200(sp)
+    sd x28, 208(sp)
+    sd x29, 216(sp)
+    sd x30, 224(sp)
+    sd x31, 232(sp)
+    # Saving mcause and mepc instead of free padding 
+    csrr t0, mcause
+    sd t0, 240(sp)
+    csrr t0, mepc
+    sd t0, 248(sp)
+    mv a0, sp               # writing the stack pointer in a0 because is the register for the function arguments
+    call jny_dispatcher   
+    # trap handled restore
+    ld x1, 0(sp)
+    ld x3, 8(sp)
+    ld x4, 16(sp)
+    ld x5, 24(sp)
+    ld x6, 32(sp)
+    ld x7, 40(sp)
+    ld x8, 48(sp)
+    ld x9, 56(sp)
+    ld x10, 64(sp)
+    ld x11, 72(sp)
+    ld x12, 80(sp)
+    ld x13, 88(sp)
+    ld x14, 96(sp)
+    ld x15, 104(sp)
+    ld x16, 112(sp)
+    ld x17, 120(sp)
+    ld x18, 128(sp)
+    ld x19, 136(sp)
+    ld x20, 144(sp)
+    ld x21, 152(sp)
+    ld x22, 160(sp)
+    ld x23, 168(sp)
+    ld x24, 176(sp)
+    ld x25, 184(sp)
+    ld x26, 192(sp)
+    ld x27, 200(sp)
+    ld x28, 208(sp)
+    ld x29, 216(sp)
+    ld x30, 224(sp)
+    ld x31, 232(sp)
+    addi sp, sp, 256        # pop the frame
+    csrrw sp, mscratch, sp  # swap back: sp <- original stack, mscratch <- trap stack top
+    mret
+    
